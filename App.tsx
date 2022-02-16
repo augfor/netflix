6b9598c7523b7aac84cb958c48426a2b364fefd9
Augusto Forero <a.forarc@gmail.com>
2022-02-16 13:05:21 -0500

    Add Authentication via AWS Amplify

--- App.tsx
+++ App.tsx
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import Amplify, { Auth } from 'aws-amplify';
+// @ts-ignore
+import { withAuthenticator } from 'aws-amplify-react-native';
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
+import awsconfig from './src/aws-exports';
 
-export default function App() {
+Amplify.configure(awsconfig);
+
+function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
 
@@ -20,3 +26,5 @@ export default function App() {
     );
   }
 }
+
+export default withAuthenticator(App);
