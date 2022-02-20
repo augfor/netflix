c77103349012be77d7c8767ac38ac68a74214116
Augusto Forero <a.forarc@gmail.com>
2022-02-19 22:36:17 -0500

    Generate model files (Amplify codegen models)

--- components/HomeCategory/index.tsx
+++ components/HomeCategory/index.tsx
@@ -44,3 +44,66 @@ const HomeCategory = (props: HomeCategoryProps) => {
 };
 
 export default HomeCategory;
+
+/** Displaying data from the server */
+
+// import { useEffect, useState } from 'react';
+// import { FlatList, Image, Pressable } from 'react-native';
+// import { useNavigation } from '@react-navigation/native';
+// import { DataStore } from 'aws-amplify';
+
+// import { Text } from '../../components/Themed';
+
+// import styles from './styles';
+// import { Category, Movie } from '../../src/models';
+
+// interface HomeCategoryProps {
+//   category: Category;
+// }
+
+// const HomeCategory = (props: HomeCategoryProps) => {
+//   const { category } = props;
+
+//   const [movies, setMovies] = useState<Movie[]>([]);
+
+//   const navigation = useNavigation();
+
+//   useEffect(() => {
+//     const fetchMovies = async () => {
+//       const result = (await DataStore.query(Movie)).filter(
+//         (movie) => movie.categoryID === category.id
+//       );
+
+//       setMovies(result);
+//     };
+
+//     fetchMovies();
+//   }, []);
+
+//   const onMoviePress = (movie: Movie) => {
+//     navigation.navigate('MovieDetailsScreen', { id: movie.id });
+//   };
+
+//   return (
+//     <>
+//       <Text style={styles.title}>{category.title}</Text>
+//       <FlatList
+//         data={movies}
+//         renderItem={({ item }) => (
+//           <Pressable onPress={() => onMoviePress(item)}>
+//             <Image
+//               style={styles.image}
+//               source={{
+//                 uri: item.poster,
+//               }}
+//             />
+//           </Pressable>
+//         )}
+//         horizontal
+//         showsHorizontalScrollIndicator={false}
+//       />
+//     </>
+//   );
+// };
+
+// export default HomeCategory;
--- screens/HomeScreen/index.tsx
+++ screens/HomeScreen/index.tsx
@@ -1,12 +1,10 @@
 import { FlatList } from 'react-native';
-// import { DataStore } from 'aws-amplify';
 
 import { View } from '../../components/Themed';
 import HomeCategory from '../../components/HomeCategory';
 
 import styles from './styles';
 import categories from '../../assets/data/categories';
-// import { Category } from '../../src/models';
 
 const HomeScreen = () => {
   return (
@@ -20,3 +18,35 @@ const HomeScreen = () => {
 };
 
 export default HomeScreen;
+
+/** Displaying data from the server */
+
+// import { FlatList } from 'react-native';
+// import { useEffect, useState } from 'react';
+// import { DataStore } from 'aws-amplify';
+
+// import { View } from '../../components/Themed';
+// import HomeCategory from '../../components/HomeCategory';
+
+// import styles from './styles';
+// import { Category } from '../../src/models';
+
+// const HomeScreen = () => {
+//   const [categories, setCategories] = useState<Category[]>([]);
+
+//   useEffect(() => {
+//     const fetchCategories = async () => {
+//       setCategories(await DataStore.query(Category));
+//     };
+
+//     fetchCategories();
+//   }, []);
+
+//   return (
+//     <View style={styles.container}>
+//       <FlatList data={categories} renderItem={({ item }) => <HomeCategory category={item} />} />
+//     </View>
+//   );
+// };
+
+// export default HomeScreen;
